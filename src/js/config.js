6a3a82904beb275a9eaddee81cbb68bc806224ae
toshi-toma <toshitoma1217@gmail.com>
2019-09-18 19:07:20 +0900

    Create Sample Plugin

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+(function(PLUGIN_ID) {
+  kintone.plugin.app.getConfig(PLUGIN_ID);
+  alert('hoge');
+})(kintone.$PLUGIN_ID);
---
+++ src/js/customize.js
@@ -0,0 +1,4 @@
+(function(PLUGIN_ID) {
+  kintone.plugin.app.getConfig(PLUGIN_ID);
+  alert('Hello');
+})(kintone.$PLUGIN_ID);
